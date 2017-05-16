4c08d6ce47965c8a99234685f4a2f3f3dd22891e
throwable <ant.kuranov@gmail.com>
2017-05-16 12:10:01 +0200

    initial commit

--- toa-plugin-test/toaplugintest.js
+++ toa-plugin-test/toaplugintest.js
@@ -12,9 +12,33 @@ function getDomain(url)
     return '';
 }
 
+function syntaxHighlight(json) {
+    if (typeof json != 'string') {
+         json = JSON.stringify(json, undefined, 2);
+    }
+    json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
+    return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match) {
+        var cls = 'number';
+        if (/^"/.test(match)) {
+            if (/:$/.test(match)) {
+                cls = 'key';
+            } else {
+                cls = 'string';
+            }
+        } else if (/true|false/.test(match)) {
+            cls = 'boolean';
+        } else if (/null/.test(match)) {
+            cls = 'null';
+        }
+        return '<span class="' + cls + '">' + match + '</span>';
+    });
+}
+
 function getPostMessageData(event)
 {
     console.log("*** Message received from "+ getDomain(event.origin) + "\n", event);
+    var pre = document.getElementById("message_body");
+    pre.innerHTML = syntaxHighlight(event.data);
 };
 
 function sendPostMessageData(data)
@@ -35,8 +59,10 @@ function sendPostMessageData(data)
 };
 
 window.addEventListener("message", getPostMessageData, false);
+
 var openerLocation;
 if (window.opener != null)
     openerLocation = window.opener.document.location.toString();
+
 console.log('*** Plugin loaded, referrer: ' + document.referrer + ", openerLocation: " + openerLocation);
 sendPostMessageData('{"apiVersion": 1,"method": "ready"}');
